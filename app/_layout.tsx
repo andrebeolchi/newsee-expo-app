9f0aa811e86079a74ca0c2339eae66a52b629325
Andre Beolchi <andregarbini2001@gmail.com>
2025-05-17 16:52:28 -0300

    refactor: rename ThemeToggle component and update imports

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,15 @@
 import '~/global.css';
 
 import { DarkTheme, DefaultTheme, Theme, ThemeProvider } from '@react-navigation/native';
+import { PortalHost } from '@rn-primitives/portal';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
 import { Platform } from 'react-native';
-import { NAV_THEME } from '~/lib/constants';
-import { useColorScheme } from '~/lib/useColorScheme';
-import { PortalHost } from '@rn-primitives/portal';
-import { ThemeToggle } from '~/components/ThemeToggle';
+import { ThemeToggle } from '~/components/theme-toggle';
 import { setAndroidNavigationBar } from '~/lib/android-navigation-bar';
+import { NAV_THEME } from '~/lib/constants';
+import { useColorScheme } from '~/lib/use-color-scheme';
 
 const LIGHT_THEME: Theme = {
   ...DefaultTheme,
@@ -22,7 +22,7 @@ const DARK_THEME: Theme = {
 
 export {
   // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
+  ErrorBoundary
 } from 'expo-router';
 
 export default function RootLayout() {
--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -1,8 +1,9 @@
+import React from 'react';
 import { Pressable, View } from 'react-native';
 import { setAndroidNavigationBar } from '~/lib/android-navigation-bar';
 import { MoonStar } from '~/lib/icons/MoonStar';
 import { Sun } from '~/lib/icons/Sun';
-import { useColorScheme } from '~/lib/useColorScheme';
+import { useColorScheme } from '~/lib/use-color-scheme';
 import { cn } from '~/lib/utils';
 
 export function ThemeToggle() {
