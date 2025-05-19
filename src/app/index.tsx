343c97af54681d4e7fec4dba54447fd84bbb54c5
Andre Beolchi <andregarbini2001@gmail.com>
2025-05-19 01:20:26 -0300

    feat: set authorization header on user login

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,20 @@
 import { Redirect } from 'expo-router'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useMMKVObject } from 'react-native-mmkv'
 
+import { setAuthorizationHeader } from '~/interfaces/sdk'
 import LoginScreen from './(anon)/login'
 
 export default function GeneralLayout() {
   const [user] = useMMKVObject('user')
 
+  useEffect(() => {
+    setAuthorizationHeader(user?.token);
+  }, []);
+
   if (!user) {
     return <LoginScreen />
   }
-  
+
   return <Redirect href='/posts' />
 }
